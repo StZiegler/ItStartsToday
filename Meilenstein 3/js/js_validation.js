4e10fb348d405ca1855b4d58aa92d709a3adff27
StZiegler <Stefanie-Ziegler@hotmail.de>
2015-05-17 23:46:19 +0200

    changed: js_validation.js, Task 3.2 - StZiegler
    added cursor position and check function for chapter
    added comments
    changed notation

--- Meilenstein 3/js/js_validation.js
+++ Meilenstein 3/js/js_validation.js
@@ -1,168 +1,141 @@
-	/* function validation () {
-
-		// überprüfung von Name, Vorname & Bauchautor auf Buchstaben, falls das Feld nicht leer ist 
-		var f = document.forms["bookentry"];
-		var isError = false;
-
-		//vorname
-		if(isNotText(f.vorname.value)) {
-			isError = true;
-			document.getElementById("vorname").classList.add("error");
-		}
-
-		// nachname
-		if (isNotText(f.nachname.value)) {
-			isError = true;
-			document.getElementById("nachname").classList.add("error");
-		}
-
-		// buchautor
-		if (isNotText(f.buchautor.value)) {
-			isError = true;
-			document.getElementById("buchautor").classList.add("error");
-		}
-
-		// Überprüfung Titel des Buches; alles erlaubt --> ggf. Prüfung auf 'Ausgefüllt' 
-		if(f.buchtitel.value !== "") {
-			document.getElementById("buchtitel").classList.add("error");
-	}
-
-	// check isbn --> max 13 numbers 
-
-	if(isISBN(f.isbn.value)) {
-		isError = false;
-		document.getElementById("isbn").classList.add("error");
-
-	}
-
-
-	// check year of release --> max 4 years, not negative, not later than the current year 2015
-	if(release(f.jahr.value)) {
-		isError = false;
-		document.getElementById('jahr').classList.add("error");
-	}
-
-		// Wenn ein Fehler ist erschent folgender Text:
-
-		if (isError) {
-			alert('Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.');
-		}
-
-	}
-	// end of validation
-
-
-	// function for text
-	function isNotText(value) {
-		if (value !== "") {
-			return value.search(/^[a-zA-Z]+$/) === -1;
-		}
-		else{
-			return true;
-		}
-	} */
-
-/* _________________________________________________________________________________________________________________ */
-
-// ********** new ************* 
 // start of validation function
-
 function validation () {
 
-	/* TODO: removal of required fields in Milestone 2*/
-	/* TODO: cursor position */
-	/* TODO: testing */
-
-
-	// check form "bookentry" 
 	var f = document.forms["bookentry"];
 	var isError = false;
+	var errorCounter = 0;
 
-	// check fields name, vorname and buchautor of correctness 
-
-		if(isNotText(f.vorname.value)) { // run isNotText function
+	// buchtitel
+	if (f.buchtitel.value == "") {
 			isError = true;
-			document.getElementById("vorname").classList.add("error"); // edge field in red for error
+			// add red border fields with wrong content
+			f.buchtitel.classList.add("error");
+			// increment fields for curser position
+			errorCounter++;
+			cursorPos(errorCounter, f.buchtitel);
 		}
 
-		// nachname
-		if (isNotText(f.nachname.value)) {
+	// buchautor
+	if (isNotText(f.buchautor.value)) {
 			isError = true;
-			document.getElementById("nachname").classList.add("error");
+			f.buchautor.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.buchautor);
 		}
 
-		// buchautor
-		if (isNotText(f.buchautor.value)) {
-			isError = true;
-			document.getElementById("buchautor").classList.add("error");
-		}
-
-		// check titel des buches; there are no wrong inputs --> letters, numbers additional characters are allowed
-
-		if(f.buchtitel.value !== "") {
-			document.getElementById("buchtitel").classList.add("error");
-
-		//check isbn number run isbn function
-
+	//isbn number
 	if(isNotISBN(f.isbn.value)) {
-		isError = true;
-		document.getElementById("isbn").classList.add("error");
+			isError = true;
+			f.isbn.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.isbn);
+		}
 
-	}
+	// kapitel
+	if (isNotCap(f.kapitel.value)) {
+			isError = true;
+			f.kapitel.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.kapitel);
+		}
 
-	// check year of release --> max 4 years, not negative, not later than the current year 2015
+	// jahr
+	if (isNotRelease(f.jahr.value)) {
+			isError = true;
+			f.jahr.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.jahr);
+		}
 
-	if(release(f.jahr.value >=0 && f.jahr.value <=2015)) {
-		isError = true;
-		document.getElementById('jahr').classList.add("error");
-	}
+	//auflage
+	if (isNotCirc(f.auflage.value)) {
+			isError = true;
+			f.auflage.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.auflage);
+		}
 
-	if(circ(f.auflage.value >=0)) {
-		isError=true;
-		document.getElementById("auflage").classList.add("error");
-	}
+	//vorname
+	if (isNotText(f.vorname.value)) { 
+			isError = true;
+			f.vorname.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.vorname);
+		}
 
-	// show error text in window
+	// nachname
+	if (isNotText(f.nachname.value)) {
+			isError = true;
+			f.nachname.classList.add("error");
+			errorCounter++;
+			cursorPos(errorCounter, f.nachname);
+		}
+
+	// show error message in window
 	if (isError) {
 			alert('Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.');
+		return false;
 		}
-
+		else {
+			return true;
+		}
 	}
-
 // end validation
 
+// functions; 
+// check name, vorname, buchautor 
+
 function isNotText(value) {
-    if (value !== "") { // check if entry is empty
-        return !/^[a-zA-Z]+$/.test(value); // check if value is letter and negate the result 
+	// check if field is empty
+    if (value != "") { 
+    	// only letters are allowed
+        return !/^[a-zA-Z]+$/.test(value); 
     }
-    else{
         return true;
-    }
 }
 
+// check isbn --> only 13 digits are allowed 
 function isNotISBN(value) {
-    if (value !== "") { // check if entry is empty 
-        return !/^[0-9]{13}$/.test(value); // check if value is number of length 13 and negate the result 
+    if (value != "") { 
+        return !/^[0-9]{13}$/.test(value); 
     }
-    else{
         return true;
-    }
 }
-
-function release (value) {
-	if (value!=="") { // check if entry is empty 
-		return !/^[0-9]{4}$/.test(value); // check if year is a number of length 4 and not bigger than 2015 / smaller than 0
-	} else {
-		return true;
+// check jahr --> 
+function isNotRelease (value) {
+	if (value != "") { 
+		// not smaller than 1000 and not bigger than 2015
+		if(value >=1000 && value <=2015) {
+			// numberlength = 4  
+			return !/^[0-9]{4}$/.test(value);
+		}
 	}
+	return true;
 }
 
-	// check whitespace???? TODO
-function circ (value) {
-	if(value!=="") {
+// check auflage
+function isNotCirc (value) {
+	if(value != "") {
+		if (value >0) {
 		return !/^[0-9]+$/.test(value);
 	}
-	else {
+		}
 		return true;
+}
+
+// check kapitel 
+function isNotCap (value) {
+	if(value !="") {
+		if(value >0) {
+			return !/[0-9]+(\.[0-9][0-9]?)?/.test(value);
+		}
+	}
+	return true;
+}
+
+// place cursor on the first field with an error
+function cursorPos (errorCounter, element) {
+	if(errorCounter == 1) {
+		element.focus();
 	}
 }
