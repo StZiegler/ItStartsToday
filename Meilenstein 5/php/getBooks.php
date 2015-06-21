f48ad39114949b6aab0d3cf3dd0a47f4fecad1fa
Ralf Langlotz <1228662@stud.hs-mannheim.de>
2015-06-21 02:39:33 +0200

    added: Meilenstein 5 - getBooks.php - RalfLanglotz
    header('Access-Control-Allow-Origin: *')

--- Meilenstein 5/php/getBooks.php
+++ Meilenstein 5/php/getBooks.php
@@ -1,22 +1,26 @@
 <?php
 
-	$horror_books_file = '../json/horror_books.json';
-	$roman_books_file = '../json/roman_books.json';
+$horror_books_file = "../json/horror_books.json";
+$roman_books_file = "../json/roman_books.json";
 
-	// get category from GET-request
-	$category = $_GET["category"];
+// get category from GET-request
+$category = $_GET["category"];
 
-	if ($category == "horror") {
-		$data = file_get_contents($horror_books_file);
-	} 
+if ($category == "horror") {
+    $data = file_get_contents($horror_books_file);
+}
 
-	if ($category == "roman") {
-		$data = file_get_contents($roman_books_file);
-	}
+if ($category == "roman") {
+    $data = file_get_contents($roman_books_file);
+}
 
-	if ($data != null && $data != false) {
-		header('Content-Type: application/json');
-		echo $data;
-	}
+if ($data != null && $data != false) {
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+    header('Access-Control-Allow-Credentials: true');
+    echo $data;
+}
 
 ?>
