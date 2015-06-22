87655be6d562382048b9b5d5e163e6c76f9b5907
StZiegler <Stefanie-ziegler@hotmail.de>
2015-06-22 15:49:54 +0200

    changed: createTable.php - Task 6.2 - StZiegler
    fixed database error mssg

--- Meilenstein 6/php/database/createTable.php
+++ Meilenstein 6/php/database/createTable.php
@@ -1,8 +1,23 @@
 <?php
 
+// sql to create table User
+$sql = "CREATE TABLE benutzerInfo (
+id INT(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+vorname VARCHAR(30) NOT NULL,
+nachname VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+)ENGINE = InnoDB";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
 // sql to create table book
 $sql = "CREATE TABLE book (
-book_id INT(6) AUTO_INCREMENT NOT NULL,
+id INT(6) UNSIGNED AUTO_INCREMENT NOT NULL,
+user_id INT(6) UNSIGNED NOT NULL,
 autor VARCHAR(30) NOT NULL,
 titel VARCHAR(30) NOT NULL,
 kapitel INT(10) NOT NULL ,
@@ -16,8 +31,12 @@ isKrimi INT (1) NOT NULL,
 isDoku INT (1) NOT NULL,
 isKomoedie INT (1) NOT NULL,
 isRoman INT (1) NOT NULL,
-PRIMARY KEY (book_id)
-)ENGINE = InnoDB;";
+PRIMARY KEY (id),
+UNIQUE (isbn, user_id),
+FOREIGN KEY (user_id) REFERENCES benutzerInfo(id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+)ENGINE = InnoDB";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table created successfully";
@@ -25,15 +44,17 @@ if ($conn->query($sql) === TRUE) {
     echo "Error creating table: " . $conn->error;
 }
 
-
 // sql to create table User
-$sql = "CREATE TABLE benutzerInfo (
-user_id INT(6) AUTO_INCREMENT NOT NULL,
-vorname VARCHAR(30) NOT NULL,
-nachname VARCHAR(30) NOT NULL,
-favorit INT(1) NOT NULL,
-PRIMARY KEY (user_id)
-)ENGINE = InnoDB;";
+$sql = "CREATE TABLE favorit (
+user_id INT(6) UNSIGNED NOT NULL,
+book_id INT(6) UNSIGNED NOT NULL,
+FOREIGN KEY (user_id) REFERENCES benutzerInfo(id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+FOREIGN KEY (book_id) REFERENCES book(id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+)ENGINE = InnoDB";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table created successfully";
